8b0a1bfa8f63e9b671c1d517cf539cda85220a6f
Seyma Ozturk <seymaarslan217@gmail.com>
2023-10-18 21:17:24 +0300

    add tailwind

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 
 function App() {
   return (
-    <>
+    <div className="bg-red-500">
 
+      <h1 className="text-3xl font-bold underline bg-red-500">
+        Hello world!
+      </h1>
 
-
-    </>
+    </div>
   );
 }
 
